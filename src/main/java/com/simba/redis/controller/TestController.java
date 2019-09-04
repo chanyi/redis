4665db1e5505dbe33c1b80e68a5b712ed8b273c9
lilei <791814697@qq.com>
2019-09-04 10:14:05 +0800

    feat(bloom):新增布隆过滤器

--- src/main/java/com/simba/redis/controller/TestController.java
+++ src/main/java/com/simba/redis/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.simba.redis.controller;
 
+import com.google.common.base.Charsets;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
 import com.simba.redis.model.Subscriber;
 import com.simba.redis.service.LuaScript;
 import com.simba.redis.service.PubSub;
@@ -12,6 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import redis.clients.jedis.Jedis;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/test")
@@ -60,5 +66,29 @@ public class TestController {
 		//lua脚本，可以定义变量也可以写入文本文件中
 		String script = "";
 		luaScript.doScript(script,2,"test1","test2");
+	}
+
+	@Test
+	@RequestMapping("/testBloomFilter")
+	public  void testBloomFilter(){
+		logger.info("测试bloom算法-->");
+		//初始化bloom过滤器，将自己的数据存储在bloom中
+		List<String> list = new ArrayList<String>();//自定义的数据
+		Double tpp = 0.0003;//误判率，1万个由三个误判
+		BloomFilter bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charsets.UTF_8),list.size(),tpp);
+		for(int i = 0;i<list.size();i++){
+			bloomFilter.put(list.get(i));
+		}
+		int count =0;
+		//验证一个key是否存在bloomFilter里
+		for(int i=0;i<list.size();i++){
+			String testKey = "testKey"+i;
+			if(bloomFilter.mightContain(testKey)){
+				//过滤器检测到可能包含
+				count ++;
+			}
+		}
+		//计算出多少个可能存在
+
 	}
 }
